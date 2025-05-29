6febdb628f9776d36b76400ffd5d97e638e1212f
Moaz <moaz.aws217@gmail.com>
2025-05-30 00:46:54 +0500

    added

--- demo.py
+++ demo.py
@@ -1,23 +1,8 @@
-def my_function():
+def hello():
+    """
+    this is demo
+    """
+    return "Hello Dosto"
 
 
-    a=17
-    b=13
-    c=65
-    g = 68
-    hg = 54
-
-
-
-    return 43
-    return 65
-    return 777
-    return 78
-    return 87
-    return 64
-    return 76
-
-
-
-
-my_function()
+hello()
